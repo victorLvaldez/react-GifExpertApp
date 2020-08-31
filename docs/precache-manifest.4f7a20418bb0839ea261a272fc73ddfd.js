5a8ccfff6a561e9ff649d353813aa9b4de49ada7
Victor Lopez <victorrubenlopezvaldez@gmail.com>
2020-08-30 19:37:01 -0500

    docs agregado

---
+++ docs/precache-manifest.4f7a20418bb0839ea261a272fc73ddfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b573e7363c2e8dbfc7f898db97f309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a75972ab985e916c017",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e074048e9f5e2ae3e621",
+    "url": "/static/css/main.0d7476ec.chunk.css"
+  },
+  {
+    "revision": "9a75972ab985e916c017",
+    "url": "/static/js/2.eca02426.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/static/js/2.eca02426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e074048e9f5e2ae3e621",
+    "url": "/static/js/main.6a1e165e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
